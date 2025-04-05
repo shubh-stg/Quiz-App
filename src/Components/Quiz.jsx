e523cb1eaff9dcd0155d02b6e29ec9ffbad9a1cc
shubh-stg <shubhstg555@gmail.com>
2025-04-05 11:21:00 +0530

    Basic structure of the Quiz App

---
+++ src/Components/Quiz.jsx
@@ -0,0 +1,50 @@
+import styles from "./Quiz.module.css"
+import {data} from "../assets/Data"
+import { useRef, useState } from "react"
+const Quiz=()=>{
+
+const [index,setindex]=useState(2); //to maintain question number
+const [question,setquestion]=useState(data[index]); //to display the question correcpoding to tthe current index
+const[lock ,setlock]=useState(false); //for selecting a single option
+
+  const Option1=useRef();
+  const Option2=useRef();
+  const Option3=useRef();
+  const Option4=useRef();
+
+  const option_array= [Option1,Option2,Option3,Option4];
+
+
+const checkans=(event,givenanswer,question)=>{
+  if(lock===false){
+    if(question.ans===givenanswer){
+    
+      event.target.classList.add("correct");
+      setlock(true);
+    }
+    else{
+      event.target.classList.add("wrong"); 
+      setlock(true);
+      option_array[question.ans-1].current.classList.add("correct");
+    }
+  }
+
+}
+
+ return <div className={styles.container}>
+
+  <h1>QUIZ APP</h1>
+  <hr className={styles.hr}/>
+  <h2 className={styles.ques}>{index+1} . {question.question}</h2>
+  <ul>
+    <li ref={Option1} onClick={(e)=>checkans(e,1,question)}>{question.option1}</li>
+    <li ref={Option2} onClick={(e)=>checkans(e,2,question)}>{question.option2}</li>
+    <li ref={Option3} onClick={(e)=>checkans(e,3,question)}>{question.option3}</li>
+    <li ref={Option4} onClick={(e)=>checkans(e,4,question)}>{question.option4}</li>
+  </ul>
+  <button>Next</button>
+  <div className={styles.index}>1 of 5 questions</div>
+
+ </div>
+}
+export default Quiz;
